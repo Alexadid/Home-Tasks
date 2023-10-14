2768e3fa54a7519eae6da2bb36fb5b991a8fa000
alexadid <fedosenko.as@phystech.edu>
2023-10-14 20:05:01 +0300

    Corrected task 3: correct number of bits for pages field, status field is bool now

--- autumn/HW4-23.09.23/task3.cpp
+++ autumn/HW4-23.09.23/task3.cpp
@@ -2,17 +2,27 @@
 #include <bitset>
 #include <string>
 
+const unsigned int bookIdBitLimit = 0x3FFF;
+const unsigned int yearBitLimit = 0xFFF;
+const unsigned int monthBitLimit = 0xF;
+const unsigned int dayBitLimit = 0x1F;
+const unsigned int numSectionsBitLimit = 0xF;
+const unsigned int numChaptersBitLimit = 0x7F;
+const unsigned int numPagesBitLimit = 0x7FF;
+
 // Определение сткруктуры для хранения информации о книгах с использованием битовых полей
 struct BookInfo
 {
     unsigned int bookId : 14;      // 0-10000, требуется 14 бит
-    unsigned int status : 1;       // 0 или 1, находится в библиотеке или на руках у читателя
+    bool status;                   // находится в библиотеке или на руках у читателя
     unsigned int year : 12;        // Год (1990-4095), требуется 12 бит
     unsigned int month : 4;        // Месяц (1-12), требуется 4 бита
     unsigned int day : 5;          // День (1-31), требуется 5 бит
     unsigned int numSections : 4;  // 0-10, требуется 4 бита
     unsigned int numChapters : 7;  // 0-100, требуется 7 бит
-    unsigned int numPages : 12;    // 0-2000, треубется 12 бит
+    // Исправил количество бит, выделенное для количества страниц
+    // Для хранения числа не превышающего 2000 хватает 11 бит (2047_10)
+    unsigned int numPages : 11;    // 0-2000, треубется 11 бит
 };
 
 void InputBookInfo(BookInfo&);
@@ -45,41 +55,42 @@ int main()
 void InputBookInfo(BookInfo& book)
 {
     unsigned int tempValue;
+    bool tempValueBool;
 
     std::cout << "Please enter book information:" << std::endl;
     std::cout << "Book ID (0-10000): ";
     std::cin >> tempValue;
     // Здесь и далее удостоверяемся, что количество бит будет корректным
-    book.bookId = tempValue & 0x3FFF;
+    book.bookId = tempValue & bookIdBitLimit;
 
     std::cout << "Status (0 - In Library, 1 - In Hands of Reader): ";
-    std::cin >> tempValue;
-    book.status = tempValue & 0x1;
+    std::cin >> tempValueBool;
+    book.status = tempValueBool;
 
     std::cout << "Receipt Date (YYYY-MM-DD)." << std::endl;
     std::cout << "Year (1990-4095): ";
     std::cin >> tempValue;
-    book.year = tempValue & 0xFFF;
+    book.year = tempValue & yearBitLimit;
 
     std::cout << "Month (1-12): ";
     std::cin >> tempValue;
-    book.month = tempValue & 0xF;
+    book.month = tempValue & monthBitLimit;
 
     std::cout << "Day (1-31): ";
     std::cin >> tempValue;
-    book.day = tempValue & 0x1F;
+    book.day = tempValue & dayBitLimit;
 
     std::cout << "Number of Sections (0-10): ";
     std::cin >> tempValue;
-    book.numSections = tempValue & 0xF;
+    book.numSections = tempValue & numSectionsBitLimit;
 
     std::cout << "Number of Chapters (0-100): ";
     std::cin >> tempValue;
-    book.numChapters = tempValue & 0x7F;
+    book.numChapters = tempValue & numChaptersBitLimit;
 
     std::cout << "Number of Pages (0-2000): ";
     std::cin >> tempValue;
-    book.numPages = tempValue & 0xFFF;
+    book.numPages = tempValue & numPagesBitLimit;
 }
 
 // Функция для проверки корректности введённой даты (по отношению к нашей библиотеке в том числе)
