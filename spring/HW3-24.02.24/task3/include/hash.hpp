ab599947451c1972699599d23761a36a43cd7b98
alexadid <fedosenko.as@phystech.edu>
2024-03-02 14:40:07 +0300

    Added all hash functions into a separate file

---
+++ spring/HW3-24.02.24/task3/include/hash.hpp
@@ -0,0 +1,15 @@
+#ifndef HASH_HPP
+#define HASH_HPP
+
+// Хэш-функции взяты отсюда: https://www.partow.net/programming/hashfunctions/#JSHashFunction
+unsigned int RSHash(const char* str, unsigned int length);
+unsigned int JSHash(const char* str, unsigned int length);
+unsigned int PJWHash(const char* str, unsigned int length);
+unsigned int ELFHash(const char* str, unsigned int length);
+unsigned int BKDRHash(const char* str, unsigned int length);
+unsigned int SDBMHash(const char* str, unsigned int length);
+unsigned int DJBHash(const char* str, unsigned int length);
+unsigned int DEKHash(const char* str, unsigned int length);
+unsigned int APHash(const char* str, unsigned int length);
+
+#endif // HASH_HPP
