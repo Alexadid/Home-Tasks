ed09c6ae6bb2013144690b6be8fede3ab3b13cf7
alexadid <fedosenko.as@phystech.edu>
2024-03-16 18:49:02 +0300

    A test file for, well, testing the source file

---
+++ spring/HW5-09.03.24/task2/fileToCorrect.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //
+// comment
+int main(/*comment*/)
+{ /**/
+	std::cout << "Hello, \"\"M\" /* commented */ world! // again :)" << std::endl; // comment
+	// comment	
+	int x; /* comment_1
+	comment_2 */	
+	int y; 
+    /*$ Я решил добавить прямо сюда проверку для 'особых' комментариев*/
+    //@ Дело в том, что мне стало лень писать отдельный файл...
+    //$ Как-то так)
+} //
+/**/
