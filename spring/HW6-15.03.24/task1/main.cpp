a605ef6bac1553ced8328464b5e6992c6b4f14b1
alexadid <fedosenko.as@phystech.edu>
2024-04-26 22:22:47 +0300

    Corrected to include functionality for more file system objects

--- spring/HW6-15.03.24/task1/main.cpp
+++ spring/HW6-15.03.24/task1/main.cpp
@@ -3,9 +3,7 @@
 #include <chrono>
 #include <iomanip>
 
-
-
-// Для некоторого улучшения читаемости кода
+// Improved readability by using a namespace alias
 namespace fs = std::filesystem;
 
 int main()
@@ -22,19 +20,45 @@ int main()
         return 1;
     }
 
-    // Итерируемся по всем объектам в директории
     for (const auto& entry : fs::directory_iterator(directoryPath))
     {
         auto fileName = entry.path().filename();
-        auto fileType = entry.is_directory() ? "Directory" : "File";
-        
+
+        // Определите тип обхекта файловой системы
+        std::string fileType;
+        if (entry.is_directory())
+        {
+            fileType = "Directory";
+        } else if (entry.is_regular_file())
+        {
+            fileType = "File";
+        } else if (entry.is_symlink())
+        {
+            fileType = "Symbolic Link";
+        } else if (entry.is_block_file())
+        {
+            fileType = "Block Device";
+        } else if (entry.is_character_file())
+        {
+            fileType = "Character Device";
+        } else if (entry.is_fifo())
+        {
+            fileType = "FIFO (Named Pipe)";
+        } else if (entry.is_socket())
+        {
+            fileType = "Socket";
+        } else
+        {
+            fileType = "Unknown";
+        }
+
         // Получаем время последнего изменения
         auto lastWriteTime = fs::last_write_time(entry);
         auto systemTimePoint = std::chrono::time_point_cast<std::chrono::system_clock::duration>(
             lastWriteTime - fs::file_time_type::clock::now() + std::chrono::system_clock::now());
 
         // Переводим в системное время
-        auto convertedTime = std::chrono::system_clock::to_time_t(systemTimePoint); 
+        auto convertedTime = std::chrono::system_clock::to_time_t(systemTimePoint);
 
         // Выводим полученную информацию в консоль
         std::cout << "Name: " << fileName << ";\n" << "Type: " << fileType << ";\n"
